8f60f0f771624b817ae0ce2ef52cfc65695a9930
Samantha Roderigues <sroderigues@umassd.edu>
2015-03-20 16:28:16 -0400

    Appliance naming/beginning schedules
    appliances now have default names, that increment so they won't all be
    the same if the user doesn't give them specialized names.
    
    Began to add scheduling section

--- src/js/appliance-list.js
+++ src/js/appliance-list.js
@@ -1,88 +1,130 @@
-defaultApplianceList = [
+availableApplianceList = [
 	{
 		type: 'Light Bulb',
-		watts: '60'
+		watts: '12000',
+		priority: '1',
+		schID: 'kitch',
 	},
 	{
 		type: 'Refrigerator',
 		watts: '615',
-		gallons: '23'
+		gallons: '23',
+		priority: '1',
+		schID: 'kitch'
 	},
 	{
 		type: 'Microwave',
-		watts: '1050'
+		watts: '1050',
+		priority: '3',
+		schID: 'kitch'
 	},
 	{
 		type: 'Television',
-		watts: '140'
+		watts: '140',
+		priority: '2',
+		schID: 'lRoom'
 	},
 	{
 		type: 'Ceiling fan',
-		watts: '75'
+		watts: '75',
+		priority: '3',
+		schID: 'lRoom'
 	},
 	{
 		type: 'Clothes Dryer',
-		watts: '3600'
+		watts: '3600',
+		priority: '2',
+		schID: 'basement'
 	},
 	{
 		type: 'Desktop Computer',
-		watts: '200'
+		watts: '200',
+		priority: '3',
+		schID: 'lRoom'	
 	},
 	{
 		type: 'Dish Washer',
-		watts: '1450'
+		watts: '1450',
+		priority: '2',
+		schID: 'kitch'
 	},
 	{
 		type: 'Electric Mower',
-		watts: '1500'
+		watts: '1500',
+		priority: '3',
+		schID: 'basement'
 	},
 	{
 		type: 'Blow Dryer',
-		watts: '1315'
+		watts: '1315',
+		priority: '3',
+		schID: 'bath'
 	},
 	{
 		type: 'Air Conditioner',
-		watts: '1080'
+		watts: '1080',
+		priority: '2',
+		schID: 'lRoom'
 	},
 	{
 		type: 'Iron',
-		watts: '1095'
+		watts: '1095',
+		priority: '3',
+		schID: 'bath'
 	},
 	{
 		type: 'Oven',
-		watts: '2540'
+		watts: '2540',
+		priority: '1',
+		schID: 'kitch'
 	},
 	{
 		type: 'Toaster',
-		watts: '1080'
+		watts: '1080',
+		priority: '3',
+		schID: 'kitch'
 	},
 	{
 		type: 'Vacuum Cleaner',
-		watts: '1010'
+		watts: '1010',
+		priority: '3',
+		schID: 'lRoom'
 	},
 	{
 		type: 'Washing Machine',
-		watts: '1275'
+		watts: '1275',
+		priority: '1',
+		schID: 'basement'
 	},
 	{
 		type: 'Coffee Machine',
-		watts: '1035'
+		watts: '1035',
+		priority: '2',
+		schID: 'kitch'
 	},
 	{
 		type: 'Water Heater',
-		watts: '4530'
+		watts: '4530',
+		priority: '1',
+		schID: 'basement'
 	},
 	{
 		type: 'Portable Heater',
-		watts: '1560'
+		watts: '1560',
+		priority: '2',
+		schID: 'lRoom'
 	},
 	{
 		type: 'Electric Furnace',
-		watts: '11830'
+		watts: '11830',
+		priority: '1',
+		schID: 'basement'
 	},
 	{
 		type: 'Central Air Conditioner',
-		watts: '3830'
+		watts: '3830',
+		priority: '2',
+		schID: 'basement'
 	}
 	
 ]
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,89 @@
 /*
 	OnClick function for Add/edit Appliances Button
 */
+
+var appArray = []; // global array for user appliances
+var arr=[];
+var arr2=[];
+var daySlotCounter=[]; //keeps track of number of slots for each day, need to save this to list
+
+/*
+	save/edit appliance
+*/
+function addSchedule()
+{
+	//var node = document.createElement('li');
+	//node.appendChild(document.createTextNode(labelValue));
+	//node.setAttribute('id', addAppliance.counter);
+}
+
+/*
+	creates a new time slot for that day
+*/
+function addTimeSlot(day)
+{
+	if(day=="sunday")
+		index = 0;
+	else if(day=="monday")
+		index = 1;
+	else if(day=="tuesday")
+		index = 2;
+	else if(day=="wednesday")
+		index = 3;
+	else if(day=="thursday")
+		index = 4;
+	else if(day=="friday")
+		index = 5;
+	else
+		index = 6;
+
+	if(daySlotCounter[index]==undefined)
+		daySlotCounter[index]=0;
+
+	idStart=day+""+daySlotCounter[index]+"start";
+	idEnd=day+""+daySlotCounter[index]+"end";
+
+	document.getElementById(day+"spot").innerHTML +="<div></div>"+day+":<input type='text' id=idStart><input type='text' id=idEnd>"
+
+	daySlotCounter[index]++;
+	
+}
+
+/*
+	creates a new schedule
+*/
+function openScheduleDescription(id)
+{ 
+	document.getElementById('schedule-section').innerHTML = "Name: <input type='text' id='name-schedule' name='namesched'><div></div>"
+	+ "<button id='sunday' onclick='addTimeSlot(id)'>Sunday</button><div id='sundayspot'></div>"
+	+ "<button id='monday' onclick='addTimeSlot(id)'>Monday</button><div id='mondayspot'></div>"
+	+ "<button id='tuesday' onclick='addTimeSlot(id)'>Tuesday</button><div id='tuesdayspot'></div>"
+	+ "<button id='wednesday' onclick='addTimeSlot(id)'>Wednesday</button><div id='wednesdayspot'></div>"
+	+ "<button id='thursday' onclick='addTimeSlot(id)'>Thursday</button><div id='thursdayspot'></div>"
+	+ "<button id='friday' onclick='addTimeSlot(id)'>Friday</button><div id='fridayspot'></div>"
+	+ "<button id='saturday' onclick='addTimeSlot(id)'>Saturday</button><div id='saturdayspot'></div>"
+	+ "<p></p><button onclick='addSchedule("+id+",false)'>Add Schedule</button>"
+}
+
+/*
+	insert our logo so they know appliance added/removed or edited properly
+*/
+function insertLogo()
+{
+	document.getElementById("appliance-description").innerHTML = "<img src='file:///C:/Users/Samantha/Documents/My%20Documents/UMDSpring2015/CIS499/seniorproject/src/logo.jpg' alt='Intelligent Home Simulator'>";
+}
+
+/*
+	OnClick function for Remove Appliances Button
+*/
+function removeAppliance(id)
+{
+		var node = document.getElementById(id);
+		var appListElem = document.getElementById('selected-appliances');
+		appListElem.removeChild(node);
+		insertLogo();
+}
+
 function addAppliance(id,edit)
 {
 	//edit = true, user is editing an appliance already existing in list
@@ -8,36 +91,62 @@ function addAppliance(id,edit)
 
 	if(!edit) //if new appliance generate new id and get its type
 	{
-		newAppliance = defaultApplianceList[id].type;
+		newAppliance = availableApplianceList[id].type;
 
 		if( typeof addAppliance.counter == 'undefined' ) 
 		   addAppliance.counter = 0;
 	}
 
+	labelValue = document.getElementsByName('labelInput')[0].value;
 	wattsValue = document.getElementsByName('wattsInput')[0].value; //get watts in input text box
 	
 	if(!edit)
 	{
 		var node = document.createElement('li');
-		node.appendChild(document.createTextNode(newAppliance));
+		node.appendChild(document.createTextNode(labelValue));
 		node.setAttribute('id', addAppliance.counter); //id in selected-appliances list should be the same as the appliance-id
 		node.setAttribute('appliance-id', addAppliance.counter); 
 		node.setAttribute('appliance-type', newAppliance);
+
+		//console.log(node.getAttribute('appliance-label'))
 	}
 	else
 	{
 		var node = document.getElementById(id);
+		node.removeChild(node.childNodes[0]);  
+		node.appendChild(document.createTextNode(labelValue));
+
 	}
 
+	node.setAttribute('appliance-label', labelValue)
 	node.setAttribute('appliance-wattage', wattsValue); //set watts in input text box to the attribute appliance-wattage
-	//node.setAttribute('appliance-gallonage', addAppliance.counter);
+
+	if((document.getElementById('applianceAttributes')).innerHTML!= "")
+	{
+		gallonsValue = document.getElementsByName('gallonsInput')[0].value;
+		node.setAttribute('appliance-gallonage', gallonsValue); 
+	}
+
 	node.onclick = function(){openApplianceDescription(this);}
-console.log(node.getAttribute('appliance-type'))
-console.log(node.getAttribute('id'))
+
 	var appListElem = document.getElementById('selected-appliances');
 	appListElem.appendChild(node);
 	addAppliance.counter++;
-	//writeToFile();
+
+	insertLogo();
+
+	if(!edit)
+		checkName(node.getAttribute('appliance-type'),true)
+
+	// putting the appliances with attributes in array
+	appArray.push([node.getAttribute('id'), node.getAttribute('appliance-type'), node.getAttribute('appliance-wattage')]);
+	// putting the array values into local storage
+	if(!window.localStorage) alert("Sorry, you're using an ancient browser");
+	else 
+	{
+    	localStorage.myArray = JSON.stringify(TheArray);
+	}
+
 }
 
 /*
@@ -49,22 +158,28 @@ function openApplianceDescription(elem)
 	var defaultGallons;
 	var change = true;
 
+	//replace logo with appliance description section
+	document.getElementById('appliance-description').innerHTML = "<div class='section-header'>Appliance Description</div><img id='large-appliance-image'/><ul><li id='appliance-type'></li><li id='appliance-labeling'></li><li id='appliance-watts'></li><div id='applianceAttributes'></div><div id='appliance-schedule'></div></ul>"
+	+"<div id='addApplianceButtonArea'></div>";
+	
 	var id = elem.getAttribute('appliance-id');
-	document.getElementById('appliance-name').innerHTML = elem.getAttribute('appliance-type');
+	document.getElementById('appliance-type').innerHTML = elem.getAttribute('appliance-type');
+	document.getElementById('appliance-labeling').innerHTML = elem.getAttribute('appliance-label');
+	
 	defaultWatts = elem.getAttribute('appliance-wattage');
-	//console.log(defaultWatts);
 	defaultGallons = elem.getAttribute('appliance-gallonage');
-//console.log(elem.getAttribute('appliance-wattage')==null)
-
+	
 	if(defaultWatts==null) //if new item add to selected-appliances
 	{
 		change = false;
-		//console.log("yes")
-		document.getElementById('appliance-name').innerHTML = defaultApplianceList[id].type;	//all appliances should have name
-		defaultWatts = defaultApplianceList[id].watts;
-		defaultGallons = defaultApplianceList[id].gallons;
+		document.getElementById('appliance-type').innerHTML = availableApplianceList[id].type;	//all appliances should have name
+		elem.setAttribute('appliance-label', checkName(availableApplianceList[id].type,false))
+
+		defaultWatts = availableApplianceList[id].watts;
+		defaultGallons = availableApplianceList[id].gallons;
 	}
 
+	document.getElementById('appliance-labeling').innerHTML = "Name: <input type='text' id='appliance-labeling' name='labelInput' value='"+elem.getAttribute('appliance-label')+"'>"
 	document.getElementById('appliance-watts').innerHTML = "<input type='text' id='appliance-watts' name='wattsInput' value='"+defaultWatts+"'> Watts"	//all appliances should have wattage/energy consumption
 
 	if(defaultGallons!=undefined && defaultGallons!=null)		//checks if appliance has gallons WILL HAVE TO ADD FOR OTHER ATTRIBUTES
@@ -74,10 +189,12 @@ function openApplianceDescription(elem)
 	else 	//resets attributes for next appliance clicked
 		applianceAttributes.innerHTML = "";
 
+	document.getElementById('appliance-schedule').innerHTML = "<select><option value='volvo'>Volvo</option><option value='saab'>Saab</option><option value='opel'>Opel</option><option value='audi'>Audi</option></select>"
+
 	if(!change)
-		addApplianceButtonArea.innerHTML = "<button onclick='addAppliance("+id+",false)'>Add Appliance</button>"; 	//adds appliance once Add Appliance button clicked
+		addApplianceButtonArea.innerHTML = "<button onclick='addAppliance("+id+",false)'>Add Appliance</button><p></p>"; 	//adds appliance once Add Appliance button clicked
 	else
-		addApplianceButtonArea.innerHTML = "<button onclick='addAppliance("+id+",true)'>Save Changes</button> <button onclick='addAppliance("+id+",true)'>Remove Appliance</button>"; 	//adds appliance once Add Appliance button clicked
+		addApplianceButtonArea.innerHTML = "<button onclick='addAppliance("+id+",true)'>Save Changes</button> <button onclick='removeAppliance("+id+",true)'>Remove Appliance</button><p></p>"; 	//adds appliance once Add Appliance button clicked
 }
 
 /*
@@ -85,35 +202,57 @@ function openApplianceDescription(elem)
 */
 function loadDefaultAppliances()
 {
-	//CHECKvar arr=[];
-	//CHECKvar arr2=[];
+	insertLogo();
 
 	var appListElem = document.getElementById('available-appliances');
-			for(var i = 0; i<defaultApplianceList.length; i++){
-				//CHECKarr.push(defaultApplianceList[i].type);
-				//CHECKarr2.push(0);
+			for(var i = 0; i<availableApplianceList.length; i++)
+			{	
 				var node = document.createElement('li');
-				var text = document.createTextNode(defaultApplianceList[i].type);
+				var text = document.createTextNode(availableApplianceList[i].type);
 				node.appendChild(text);
 				node.setAttribute('appliance-id', i);
+
 				node.onclick = function(){openApplianceDescription(this);}
 				appListElem.appendChild(node);
-			}
+				(availableApplianceList[i].schID)
 
-	/*CHECKfor(var j=0;j<arr.length; j++)
-	{
-		for(var k=0;k<arr.length; k++)
-		{
-			if(j!=k && arr[j]==(arr[k]))
-			{
-				arr2[j]++;
-				
 			}
-		}	
-	}
-	for(var l=0;l<arr.length; l++)
-	{
-		console.log(arr[l]);
-		console.log(arr2[l]);
-	}*/
 }
+
+function checkName(type, added)
+{
+	repeat = false;
+
+	if(document.getElementById(0)==null)
+	{
+		arr.push(type);
+		arr2.push(0)
+		name = type +" 0";
+	}
+	else
+	{ 
+		for(var j=0;j<arr.length; j++)
+		{
+			if(arr[j] == type)
+			{
+				repeat = true;
+
+				if(added)
+					arr2[j]++;
+
+				name = type +" "+arr2[j];
+				break;
+			}
+		}
+		if(!repeat)
+		{
+			arr.push(type);
+			arr2.push(0);
+			name = type +" 0";
+		}
+	}
+ //console.log(name)
+	return name;
+}
+
+
